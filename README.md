ef9428c34af28e877e177e187df3740681e6b17c
FishGoddess <1149062639@qq.com>
2022-10-01 20:45:27 +0800

    调整部分设置

--- README.md
+++ README.md
@@ -1,6 +1,4 @@
 # 🔧 Nvim
 
-[![License](_icons/license.svg)](https://opensource.org/licenses/MIT)
-
 **Nvim** 是我的 neovim 配置仓库。
 
--- lua/basic.lua
+++ lua/basic.lua
@@ -19,24 +19,18 @@ vim.wo.signcolumn = "yes"
 -- 右侧参考线，超过表示代码太长了，考虑换行
 vim.wo.colorcolumn = "100"
 
--- 缩进2个空格等于一个Tab
-vim.o.tabstop = 4
-vim.bo.tabstop = 4
-vim.o.softtabstop = 4
-vim.o.shiftround = true
-
 -- >> << 时移动长度
-vim.o.shiftwidth = 2
-vim.bo.shiftwidth = 2
+vim.o.shiftwidth = 4
+vim.bo.shiftwidth = 4
 
 -- 空格替代tab
 vim.o.expandtab = false
 vim.bo.expandtab = false
 
 -- 新行对齐当前行
-vim.o.autoindent = true
-vim.bo.autoindent = true
-vim.o.smartindent = true
+--vim.o.autoindent = true
+--vim.bo.autoindent = true
+--vim.o.smartindent = true
 
 -- 搜索大小写不敏感，除非包含大写
 vim.o.ignorecase = true
@@ -91,7 +85,7 @@ vim.o.termguicolors = true
 vim.opt.termguicolors = true
 
 -- 不可见字符的显示，这里只把空格显示为一个点
-vim.o.list = true
+--vim.o.list = true
 
 -- 补全增强
 vim.o.wildmenu = true
